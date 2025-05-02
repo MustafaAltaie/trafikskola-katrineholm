d15c3ceda4cb8f1b7520dee9220104cfe414f6fb
mustafa altaie <mustafaphoto111@gmail.com>
2025-05-02 12:40:03 +0200

    changes in style

--- frontend/src/components/Section2.jsx
+++ frontend/src/components/Section2.jsx
@@ -2,16 +2,18 @@ import '../styles/section2.css';
 
 const Section2 = () => {
     return (
-        <section className="section2">
-            <h1>Ta ditt körkort med självförtroende!</h1>
-            <div className='sec2Contact'>
-                <h3><i className="fa-solid fa-phone"></i>0737025139</h3>
-                <h3><i className="fa-solid fa-envelope"></i>mohanad@gmail.com</h3>
-            </div>
-            <div className='sec2Ads'>
-                <h3><i className="fa-solid fa-arrow-right"></i>Körkortstillstånd</h3>
-                <h3><i className="fa-solid fa-arrow-right"></i>Handledarskap</h3>
-                <h3><i className="fa-solid fa-arrow-right"></i>Plugga online</h3>
+        <section className="section2 bgLines">
+            <div>
+                <h1>Ta ditt körkort med självförtroende!</h1>
+                <div className='sec2Contact'>
+                    <h3><i className="fa-solid fa-phone"></i>0737025139</h3>
+                    <h3><i className="fa-solid fa-envelope"></i>mohanad@gmail.com</h3>
+                </div>
+                <div className='sec2Ads'>
+                    <h3><i className="fa-solid fa-arrow-right"></i>Körkortstillstånd</h3>
+                    <h3><i className="fa-solid fa-arrow-right"></i>Handledarskap</h3>
+                    <h3><i className="fa-solid fa-arrow-right"></i>Plugga online</h3>
+                </div>
             </div>
         </section>
     )
